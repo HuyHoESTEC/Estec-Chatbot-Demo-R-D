86dbefbe979dc6d35490e42ad9a11fd1b4b38595
 <huy.ho@biendongco.vn>
2025-05-16 14:16:51 +0700

    Upload init project

---
+++ estec-chatbot-demo/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ChatView from './views/ChatView.vue';
+</script>
+
+<template>
+  <div class="page">
+    <ChatView />
+  </div>
+</template>
+
+<style scoped>
+.page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+  background-image: url(../src/assets/estec-background.png);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
